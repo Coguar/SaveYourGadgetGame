d42d26b7ec4fcdbf5e90aa4f354c1a5f2aaef660
Stepan <Cogucc@mail.ru>
2016-03-07 19:01:32 +0300

    В дополнение к предидущему коммиту

---
+++ Classes/Constants.h
@@ -0,0 +1,39 @@
+#pragma once
+
+//listeners priority
+const int TOCH_ON_SPRITE_PRIORITY = 30;
+
+//fonts
+const std::string MAIN_FONT = "score.ttf";
+
+//sounds
+const std::string POINT_SOUND = "sound/Point.mp3";
+const std::string LOSE_SOUND = "sound/lose.mp3";
+const std::string DROP_SOUND = "sound/drop.mp3";
+const std::string BG_SOUND = "sound/BG_sound.mp3";
+
+//pictures
+
+//scenes backgrounds
+const std::string SPLASH_SCRENE = "SplashScene.png";
+const std::string MENU_SCRENE = "GameScene.png";
+const std::string GAMEOVER_SCRENE = "GameOverScene.png";
+
+//buttons
+const std::string PLAY_BUTTON = "play.png";
+const std::string PLAY_BUTTON_CLICKED = "PLAY_Clicked.png";
+
+//game game objects
+const std::string DEAD_LINE = "DeadLine.png";
+const std::string PHONE_ITEM = "phone.png";
+const std::string LAPTOP_ITEM = "iPad.png";
+
+//signals
+const std::string ADD_POINT = "addPoint";
+
+
+
+
+
+
+
